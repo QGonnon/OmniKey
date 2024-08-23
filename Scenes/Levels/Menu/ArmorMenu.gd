c98cb06faa1a507980980ee1aca30d4b9e7f684d
DarmonNoah <noah.darmon@ecole-isitech.fr>
2024-08-23 12:43:17 +0200

    FINISHED ALL HUB-MENU

--- Scenes/Levels/Menu/ArmorMenu.gd
+++ Scenes/Levels/Menu/ArmorMenu.gd
@@ -1,49 +1,40 @@
 extends CanvasLayer
 
-onready var light_skills_menu = get_node("/root/Level/LightSkillsMenu")
-onready var medium_skills_menu = get_node("/root/Level/MediumSkillsMenu")
-onready var heavy_skills_menu = get_node("/root/Level/HeavySkillsMenu")
+onready var light_skills_menu = $"../../LightSkillsMenu/CanvasLayer"
+onready var medium_skills_menu = $"../../MediumSkillsMenu/CanvasLayer"
+onready var heavy_skills_menu = $"../../HeavySkillsMenu/CanvasLayer"
 onready var lightArmorsButton = $Panel/VBoxContainer/LightArmorsButton
 onready var mediumArmorsButton = $Panel/VBoxContainer/MediumArmorsButton
 onready var heavyArmorsButton = $Panel/VBoxContainer/HeavyArmorsButton
 
 func _ready():
+	visible=false
 	lightArmorsButton.connect("pressed", self, "_on_LightArmorsButton_pressed")
 	mediumArmorsButton.connect("pressed", self, "_on_MediumArmorsButton_pressed")
 	heavyArmorsButton.connect("pressed", self, "_on_HeavyArmorsButton_pressed")
-	hide()
 
 func _on_LightArmorsButton_pressed():
 	if lightArmorsButton : 
-		print("print light réussit")
-		print(lightArmorsButton)
-		print(light_skills_menu)
 		hide_all_skill_menus()
-		light_skills_menu.show()
+		light_skills_menu.visible=true
 	else :
 		print("print light raté")
 
 func _on_MediumArmorsButton_pressed():
 	if mediumArmorsButton : 
-		print("print medium réussit")
-		print(mediumArmorsButton)
-		print(medium_skills_menu)
 		hide_all_skill_menus()
-		medium_skills_menu.show()
+		medium_skills_menu.visible=true
 	else :
 		print("print medium raté")
 
 func _on_HeavyArmorsButton_pressed():
 	if heavyArmorsButton : 
-		print("print heavy réussit")
-		print(heavyArmorsButton)
-		print(heavy_skills_menu)
 		hide_all_skill_menus()
-		heavy_skills_menu.show()
+		heavy_skills_menu.visible=true
 	else :
 		print("print heavy raté")
 
 func hide_all_skill_menus():
-	light_skills_menu.hide()
-	medium_skills_menu.hide()
-	heavy_skills_menu.hide()
+	light_skills_menu.visible=false
+	medium_skills_menu.visible=false
+	heavy_skills_menu.visible=false
--- Scenes/Levels/Menu/MissionMenu.gd
+++ Scenes/Levels/Menu/MissionMenu.gd
@@ -5,6 +5,7 @@ func _ready():
 	$Panel/VBoxContainer/Button1.connect("pressed", self, "_on_Button_pressed", [1])
 	$Panel/VBoxContainer/Button2.connect("pressed", self, "_on_Button_pressed", [2])
 	$Panel/VBoxContainer/Button3.connect("pressed", self, "_on_Button_pressed", [3])
+	$Panel/VBoxContainer/Button4.connect("pressed", self, "_on_Button_pressed", [4])
 	hide() # Hide the menu initially
 
 func _on_Button_pressed(value):
