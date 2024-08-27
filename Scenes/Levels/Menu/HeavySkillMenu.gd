a07570726aa83e6b7873f43ea9e8316a1a4066ac
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-08-27 19:30:24 +0200

    chore: Refactor skill button connections in skill menus
    Refactor the button connections in the HeavySkillMenu, LightSkillMenu, and MediumSkillMenu scripts to use the corresponding skill names instead of numerical values.

--- Scenes/Levels/Menu/HeavySkillMenu.gd
+++ Scenes/Levels/Menu/HeavySkillMenu.gd
@@ -1,10 +1,17 @@
 extends CanvasLayer
+onready var character = $"%Character"
 
 func _ready():
 	
-	$Panel/VBoxContainer/Button1.connect("pressed", self, "_on_Button_pressed", [1])
-	$Panel/VBoxContainer/Button2.connect("pressed", self, "_on_Button_pressed", [2])
+	$Panel/VBoxContainer/Button1.connect("pressed", self, "_on_Button_pressed", ["damageReduction"])
+	$Panel/VBoxContainer/Button2.connect("pressed", self, "_on_Button_pressed", ["offensiveShield"])
 	visible=false
 
-func _on_Button_pressed(value):
-	print(String(value) + " sélectionné")
+func _on_Button_pressed(value: String):
+	print(value + " sélectionné")
+	character.skill1.active_timer.queue_free()
+	character.skill1.cooldown_timer.queue_free()
+	
+	character.skill1 = Skill.new(value)
+	character.add_child(character.skill1.active_timer)
+	character.add_child(character.skill1.cooldown_timer)
